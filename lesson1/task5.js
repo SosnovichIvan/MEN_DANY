0258d2ff800be493a3cbac56c94d9353c3758161
Вадим Максимов <pokinet@yandex.ru>
2024-02-06 18:37:15 +0300

    Problem solving

--- lesson1/task5.js
+++ lesson1/task5.js
@@ -1,2 +1,23 @@
 // написать функцию замыкания
 // которая при первом вызове принимает массив имен и возращает каждый раз следющее имя и так по кругу
+
+let c = ["Wadim", "Ivan", "Saсha"]
+
+function main() {
+    let i = 0;
+    return function () {
+        if(i<c.length){
+        return c[i++]
+        }
+        else{i=0
+        return c[i++]}
+        }
+        
+}
+let beta = main()
+console.log(beta());
+console.log(beta());
+console.log(beta());
+console.log(beta());
+console.log(beta());
+console.log(beta());
